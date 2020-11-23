4d68ef7902326d96c3f333e88de03b1baf8440ef
m1rzaa <m1rzatanveerbeg1111@gmail.com>
2020-11-23 21:49:07 +0530

    Version 1.0

---
+++ Assets/Scripts/Movement_Script.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement_Script : MonoBehaviour {
+
+
+	private bool walking = false;
+	private Vector3 spawnpoint;
+
+	// Use this for initialization
+	void Start () {
+
+		spawnpoint = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (walking) {
+			transform.position = transform.position + Camera.main.transform.forward * 1.5f * Time.deltaTime;
+		}
+
+		if (transform.position.y < -10f) {
+			transform.position = spawnpoint;
+		}
+
+		Ray ray = Camera.main.ViewportPointToRay (new Vector3 (.5f, .5f, 0));
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.collider.name.Contains ("Plane")) {
+				walking = false;
+			} else {
+				walking = true;
+			}
+		}
+	}
+}
